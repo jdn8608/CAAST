6a30fc378e4ee7ff634a9d9a3c3a712f7a5b29d2
Joseph Nied <jdn8608@g.rit.edu>
2024-10-18 12:59:18 -0500

    Add in initial arg pass in functionality

--- pl2.py
+++ pl2.py
@@ -5,6 +5,7 @@ import random
 import pickle
 import glob
 import os
+import argparse
 
 
 
@@ -22,4 +23,22 @@ TO DO:
 '''
 
 if __name__ == "__main__":
-	quit()	
+	parser = argparse.ArgumentParser(
+		prog="RS-PL",
+		description="Remote Sensing - Pixel Label (RS-PL) tool:\n This tool was developed to have an easy, quick, and accesible tool to label imagery from various remote sensing platforms.",
+		epilog='Tool is currently under developement. For more information, got to GITHUB_LINK')
+
+	parser.add_argument('dir', 
+		help="root directory to retrieve files from")
+	parser.add_argument('files', 
+		help="files to read in. If --multiangle is off, only first file will be read",
+		nargs='+')
+	parser.add_argument('-ma', '--multiangle', 
+		help="turn on multi-angle use", 
+		action='store_true')
+	parser.add_argument('-v', '--verbose',
+		action='store_true')
+	args = parser.parse_args()
+	print(args.dir)
+	print(args.files)
+	quit()
