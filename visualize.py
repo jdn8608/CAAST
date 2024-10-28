cb958d3a33ed1a7771c3d44a11857152a53af145
Joseph Nied <jdn8608@g.rit.edu>
2024-10-28 09:47:23 -0500

    Created new folder "Widgets" and added colormap functionality to colormaps.py within
    From now on, all visualization functionality will be created in separate 'widget' scripts to ease the logic in the visualization.py script. This will also let users swap out "types" of widgets down the line...

--- visualize.py
+++ visualize.py
@@ -8,33 +8,7 @@ from matplotlib.colors import ListedColormap
 from matplotlib.colors import LinearSegmentedColormap
 
 
-def _get_custom_colormap(options, plural=False):
-
-	with open('./util_files/custom_colormaps.json', "r") as file:
-		custom_colormaps = json.load(file)
-	return {int(key):[float(item) for item in values] for key,values in custom_colormaps[options].items()}
-	
-	
-
-def get_colormap(option):
-	# get band colormap(s)	
-	colormap = option 
-	if colormap[0:7] == 'custom_':
-		colormap = _get_custom_colormap(colormap[7:])
-	return colormap
-
-def get_colormaps_from_config(config, option_name):
-	if isinstance(config[option_name], str):
-		return get_colormap(config[option_name])
-	elif isinstance(config[option_name], list):
-		return [get_colormap(opt) for opt in config[option_name]]
-	elif config[option_name] is None:
-		return None
-	else:
-		raise Exception("Erro within visualization config file: 'f{option_name}' option is of invalid type 'f{type(config['band_color_maps'])}'")
-
-def get_all_colormaps(config):
-	return tuple([get_colormaps_from_config(config, x) for x in ['band_colormaps','label_colormap','mask_colormap']])
+from widgets.colormaps import get_all_colormaps
 
 def single_view_multi_band(data, band_names, prior_labels=False, vis_config_file='./util_files/default_vizconfig.json'):
 	with open(vis_config_file, "r") as file:
@@ -56,5 +30,5 @@ def single_view_multi_band(data, band_names, prior_labels=False, vis_config_file
 	napari.run()
 	viewer.close()
 
-
+	print("\n\nClose\n\nfgjf")
 
