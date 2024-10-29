f231ab2055b17ace22a612f4bbbfce061a089107
Joseph Nied <jdn8608@g.rit.edu>
2024-10-28 20:54:23 -0500

    Finished output file saving

--- widgets/SubmitButtons.py
+++ widgets/SubmitButtons.py
@@ -7,14 +7,20 @@ from qtpy.QtWidgets import QHBoxLayout, QVBoxLayout, QGridLayout, QComboBox, QPu
 def save_labels(data, qual_dict, output_filepath, dataset_name):
 
 		filetype = output_filepath.split('.')[-1]
+		if qual_dict:
+			qual_attributes = {label: combo_box.currentText() for label, combo_box in qual_dict.items()}	
+			print(qual_attributes)
+
 		if filetype == 'npy':
 			np.save(output_filepath, data)
 			if qual_dict:
-				qual_attributes = {label: combo_box.currentText() for label, combo_box in qual_dict.items()}	
-				print(qual_attributes)
-		elif filetype == 'hdf':
+				np.save(output_filepath.split('.')[0]+'_qual.npy', qual_attributes)
+		elif filetype == 'hdf' or filtype=='hdf5':
 			out_file = h5.File(output_filepath, "w")
-			out_file.create_dataset(dataset_name, data=data)
+			h5_dataset = out_file.create_dataset(dataset_name, data=data)
+			if qual_dict:
+				for key, value in qual_attributes.items():
+					h5_dataset.attrs[key] = value
 			out_file.close()
 		elif filetype == 'nc':
 			da = xr.DataArray(
@@ -22,7 +28,10 @@ def save_labels(data, qual_dict, output_filepath, dataset_name):
 				dims=("y", "x"),
 				name=dataset_name
 			)
-			ds = xr.Dataset({dataset_name: da}, attrs=qual_attributes)
+			if qual_dict:	
+				ds = xr.Dataset({dataset_name: da}, attrs=qual_attributes)
+			else:
+				ds = xr.Dataset({dataset_name:da})
 			ds.to_netcdf(output_filepath)
 		else:
 			raise Exception(f"filetype '{filetype}' id not currently supported for saving files.\n please use a different filetype for output, or add functionality for this filetype")
