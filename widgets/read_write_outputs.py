d280cf5681199daa8e70bf934308af20fded3631
Joseph Nied <jdn8608@g.rit.edu>
2024-10-29 13:07:34 -0500

    Initial commit for adding a read_labels function

--- widgets/read_write_outputs.py
+++ widgets/read_write_outputs.py
@@ -33,3 +33,31 @@ def save_labels(data, qual_dict, output_filepath, dataset_name):
                         ds.to_netcdf(output_filepath)
                 else:
                         raise Exception(f"filetype '{filetype}' id not currently supported for saving files.\n please use a different filetype for output, or add functionality for this filetype")
+
+def read_labels(output_filepath, dataset_name):
+
+                filetype = output_filepath.split('.')[-1]
+
+                if filetype == 'npy':
+                        np.save(output_filepath, data)
+                        np.save(output_filepath.split('.')[0]+'_qual.npy', qual_attributes)
+                elif filetype == 'hdf' or filtype=='hdf5':
+                        out_file = h5.File(output_filepath, "w")
+                        h5_dataset = out_file.create_dataset(dataset_name, data=data)
+                        if qual_dict:
+                                for key, value in qual_attributes.items():
+                                        h5_dataset.attrs[key] = value
+                        out_file.close()
+                elif filetype == 'nc':
+                        da = xr.DataArray(
+                                data,
+                                dims=("y", "x"),
+                                name=dataset_name
+                        )
+                        if qual_dict:
+                                ds = xr.Dataset({dataset_name: da}, attrs=qual_attributes)
+                        else:
+                                ds = xr.Dataset({dataset_name:da})
+                        ds.to_netcdf(output_filepath)
+                else:
+                        raise Exception(f"filetype '{filetype}' id not currently supported for saving files.\n please use a different filetype for output, or add functionality for this filetype")
