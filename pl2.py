13891cb7c635738e4cd79b5cffb1922109e3089d
Joseph Nied <jdn8608@g.rit.edu>
2024-10-29 12:22:33 -0500

    Initial commit for loading prior manual labels
    Functionality that if pass-in variables are provided, AND if the output file exists, the labels in this file can be loaded as an additional layer.
    
    pretty much loading pass-saved labeling.
    
    functionality will also allow to load these or the mask in as the priors labels for the editing layer

--- pl2.py
+++ pl2.py
@@ -1,12 +1,8 @@
-
-from tqdm.notebook import tqdm
-
-import random
-import pickle
-import glob
-import os
 import argparse
+import glob
 import json
+import os
+from pathlib import Path
 
 from get_numpy_data import get_data
 from visualize import single_view_multi_band
@@ -20,15 +16,19 @@ TO DO:
 		-REQUIRMENTS.TXT
 '''
 
-def get_output_settings(json_file, input_filename):	
+def get_output_settings(json_file, input_filename, ma=False):	
 	with open(json_file, 'r') as file:
 		output_file_configs = json.load(file)
-	if output_file_configs["override_filename"]:
-		return output_file_configs["override_filename"], output_file_configs["dataset_name"]
-	elif output_file_configs["override_filepath"]:
-		return output_file_configs["override_filepath"]+input_filename.split('.')[0].split(os.sep)[-1]+output_file_configs["append_name"]+output_file_configs["file_type"], output_file_configs["dataset_name"]
+	if ma:
+		# TBD for Multi-angle functionality
+		return None, None
 	else:
-		return input_filename.split('.')[0]+output_file_configs["append_name"]+output_file_configs["file_type"], output_file_configs["dataset_name"]
+		if output_file_configs["override_filename"]:
+			return output_file_configs["override_filename"], output_file_configs["dataset_name"]
+		elif output_file_configs["override_filepath"]:
+			return output_file_configs["override_filepath"]+input_filename.split('.')[0].split(os.sep)[-1]+output_file_configs["append_name"]+output_file_configs["file_type"], output_file_configs["dataset_name"]
+		else:
+			return input_filename.split('.')[0]+output_file_configs["append_name"]+output_file_configs["file_type"], output_file_configs["dataset_name"]
 		
 
 if __name__ == "__main__":
@@ -50,27 +50,40 @@ if __name__ == "__main__":
 	parser.add_argument('-ma', '--multiangle', 
 		help="turn on multi-angle use", 
 		action='store_true')
-	parser.add_argument('-rc', '--reader_config', 
+	parser.add_argument('-r', '--reader_config', 
 		help="Path to a .json file for additional information to use by the instrument file reader, if it is needed.")
-	parser.add_argument('-vc', '--vis_config', 
+	parser.add_argument('-v', '--vis_config', 
 		help="Path to a .json file for additional information and options to use by the visualization script/software.",
 		default='./util_files/default_vizconfig.json')
-	parser.add_argument('-v', '--verbose',
+	parser.add_argument('-c', '--check_manual_labels',
+		help="Flag to check for if manual labels already exists, based on the OUTPUT_SETTINGS_FILE settings. If a file is found, these labels will be loaded as an additional layer. If no file is found, this flag does nothing.", 
+		action='store_true')
+	parser.add_argument('-l', '--load_labels',
+		help="Pass in argument to select what labels to load into the Editing layer. Values can be 'mask' or 'manual'. If not provided, Editing layer will be loaded with 1s or 0s. IF mask is selected: the file reader needs to return the mask and load_labels=True. IF manual selected, the -l parameter needs to be passed and the file needs to be detected. If either case fails, default settings of None are selected.",
+		default=None) 
+	parser.add_argument('-V', '--verbose',
 		action='store_true')
 
 	# compile args
 	args = parser.parse_args()
 	# retrieve data
-	data, band_names, load_labels, input_filename = get_data(args.dir, args.instrument_name,
+	data, band_names, prior_mask, input_filename = get_data(args.dir, args.instrument_name,
 		multiangle=args.multiangle,
 		reader_config_file=args.reader_config
 		)
+
+	output_filename, dataset_name = get_output_settings(args.output_settings_file, input_filename, ma=args.multiangle)
+
 	if not args.multiangle:
-		output_filename, dataset_name = get_output_settings(args.output_settings_file, input_filename)
+		# settings flag: if output file exists, and if labels are desired, this flag will let them to be loaded in
+		prior_manual_labels = args.check_manual_labels and Path(output_filename).is_file()
+			
 		single_view_multi_band(data=data, 
 			band_names=band_names, 
 			output_filepath=output_filename,
-			prior_labels=load_labels, 
+			prior_mask=prior_mask, 
+			prior_manual_labels=prior_manual_labels,
+			load_labels=args.load_labels,
 			dataset_name=dataset_name,
 			vis_config_file=args.vis_config)
 	else:
--- visualize.py
+++ visualize.py
@@ -2,12 +2,12 @@ import json
 import os
 import numpy as np
 import napari 
-import tqdm 
 
 from widgets.colormaps import get_all_colormaps
 from widgets.SubmitButtons import create_buttons
 
-def single_view_multi_band(data, band_names, output_filepath, prior_labels=False, dataset_name=None, vis_config_file='./util_files/default_vizconfig.json'):
+def single_view_multi_band(data, band_names, output_filepath, prior_mask=False, prior_manual_labels=False, 
+		load_labels=None, dataset_name=None, vis_config_file='./util_files/default_vizconfig.json'):
 	with open(vis_config_file, "r") as file:
 		config = json.load(file)
 
@@ -15,15 +15,24 @@ def single_view_multi_band(data, band_names, output_filepath, prior_labels=False
 
 	viewer = napari.Viewer()
 	
-	if prior_labels:
+	#if prior_manual_labels:
+	#LOAD NON-EDIT LAYER OF PRIORS -> NEED TO PASS DATA OR LOAD IT HERE		
+	if prior_mask:
 		viewer.add_image(data[:,:,:-1], name=band_names[:-1], channel_axis=2,contrast_limits=(0,1), colormap=band_colormaps)
 		orig_labels = viewer.add_labels(data[:,:,-1].astype(int), name=band_names[-1], colormap=mask_colormap )
 		orig_labels.editable = False
-		labels_layer = viewer.add_labels(data[:,:,-1].astype(int), name='Editing', colormap=label_colormap)
 	else:
 		viewer.add_image(data, name=band_names, channel_axis=2, contrast_limits=(0,1), colormap=band_colormaps)
-		labels_layer = viewer.add_labels(1+np.zeros(data[:,:,0].shape, dtype=int), name='Editing', colormap=label_colormap)
 
+	if load_labels is None:
+		labels_layer = viewer.add_labels(1+np.zeros(data[:,:,0].shape, dtype=int), name='Editing', colormap=label_colormap)
+	elif prior_mask and load_labels == "mask":
+		labels_layer = viewer.add_labels(data[:,:,-1].astype(int), name='Editing', colormap=label_colormap)
+	#elif prior_manual_labels and load_labels == "manual":
+	# lOAD PRIOR MANUAL LABELS AS THE EDITING LABELS
+	else:
+		raise Warning("load_labels settigs have ambigous settings when compare to prior_mask or prior_manual_labels variables\n defaulting to 'None' value functionality and loading zeros as the Editing Layer")
+		labels_layer = viewer.add_labels(np.zeros(data[:,:,0].shape, dtype=int), name='Editing', colormap=label_colormap)
 
 	button_location = config["button_location"]
 	qual_labels = config['qual_labels']
