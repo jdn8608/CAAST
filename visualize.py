a825b87037535541d6ea7f33e802036217bdebcc
Joseph Nied <jdn8608@g.rit.edu>
2024-11-01 12:56:01 -0500

    Added functionality to load prior manual pixel labels into a layer and into editing
    This lets users look at old labels they have created and also continue editing later
    (pick up where we left off)
    
    Need to add in loadingi "quality" drop down to widget.
    
    Also add option for versioning?

--- visualize.py
+++ visualize.py
@@ -5,22 +5,28 @@ import napari
 
 from widgets.colormaps import get_all_colormaps
 from widgets.SubmitButtons import create_buttons
+from widgets.read_write_outputs import read_labels
 
 def single_view_multi_band(data, band_names, output_filepath, prior_mask=False, prior_manual_labels=False, 
 		load_labels=None, dataset_name=None, vis_config_file='./util_files/default_vizconfig.json'):
 	with open(vis_config_file, "r") as file:
 		config = json.load(file)
 
+	button_location = config["button_location"]
+	qual_labels = config['qual_labels']
+
 	band_colormaps, label_colormap, mask_colormap = get_all_colormaps(config)
 
 	viewer = napari.Viewer()
 	
-	#if prior_manual_labels:
-	#LOAD NON-EDIT LAYER OF PRIORS -> NEED TO PASS DATA OR LOAD IT HERE		
+	if prior_manual_labels:
+		man_labels, man_qual_attrs = read_labels(output_filepath, dataset_name, qual_attrs=qual_labels)
+		man_labels_layer = viewer.add_labels(man_labels.astype(int), name="Prior Manual Labels", colormap=label_colormap )
+		man_labels_layer.editable = False
 	if prior_mask:
 		viewer.add_image(data[:,:,:-1], name=band_names[:-1], channel_axis=2,contrast_limits=(0,1), colormap=band_colormaps)
-		orig_labels = viewer.add_labels(data[:,:,-1].astype(int), name=band_names[-1], colormap=mask_colormap )
-		orig_labels.editable = False
+		orig_labels_layer = viewer.add_labels(data[:,:,-1].astype(int), name=band_names[-1], colormap=mask_colormap )
+		orig_labels_layer.editable = False
 	else:
 		viewer.add_image(data, name=band_names, channel_axis=2, contrast_limits=(0,1), colormap=band_colormaps)
 
@@ -28,14 +34,12 @@ def single_view_multi_band(data, band_names, output_filepath, prior_mask=False, 
 		labels_layer = viewer.add_labels(1+np.zeros(data[:,:,0].shape, dtype=int), name='Editing', colormap=label_colormap)
 	elif prior_mask and load_labels == "mask":
 		labels_layer = viewer.add_labels(data[:,:,-1].astype(int), name='Editing', colormap=label_colormap)
-	#elif prior_manual_labels and load_labels == "manual":
-	# lOAD PRIOR MANUAL LABELS AS THE EDITING LABELS
+	elif prior_manual_labels and load_labels == "manual":
+		labels_layer = viewer.add_labels(man_labels.astype(int), name='Editing', colormap=label_colormap)
 	else:
 		raise Warning("load_labels settigs have ambigous settings when compare to prior_mask or prior_manual_labels variables\n defaulting to 'None' value functionality and loading zeros as the Editing Layer")
 		labels_layer = viewer.add_labels(np.zeros(data[:,:,0].shape, dtype=int), name='Editing', colormap=label_colormap)
 
-	button_location = config["button_location"]
-	qual_labels = config['qual_labels']
 
 	create_buttons(viewer=viewer,
 			labels_layer=labels_layer, 
--- widgets/read_write_outputs.py
+++ widgets/read_write_outputs.py
@@ -1,8 +1,10 @@
 import numpy as np
 import h5py as h5
 import xarray as xr
+import datetime
 
 def save_labels(data, qual_dict, output_filepath, dataset_name):
+		print(f"{datetime.datetime.now()}: labels saved to {output_filepath}")
 		filetype = output_filepath.split('.')[-1]
 		if qual_dict:
 			qual_attributes = {label: combo_box.currentText() for label, combo_box in qual_dict.items()}
@@ -11,7 +13,7 @@ def save_labels(data, qual_dict, output_filepath, dataset_name):
 
 		if filetype == 'npy':
 			npy_read_write(True, output_filepath, data=data, qual_attrs=qual_attributes)
-		elif filetype == 'hdf' or filtype=='hdf5':
+		elif filetype == 'hdf' or filetype=='hdf5':
 			hdf_read_write(True, output_filepath, dataset_name, data=data, qual_attrs=qual_attributes)
 		elif filetype == 'nc':
 			nc_read_write(True, output_filepath, dataset_name, data=data, qual_attrs=qual_attributes)
@@ -23,6 +25,14 @@ def npy_read_write(write, output_filepath, data=None, qual_attrs=None):
 		np.save(output_filepath, data)
 		if qual_attrs:
 			np.save(output_filepath.split('.')[0]+'_qual.npy', qual_attrs)
+	else:
+		data = np.load(output_filepath, allow_pickle=True)
+		if qual_attrs:
+			attributes = np.load(output_filepath.split('.')[0]+'_qual.npy', allow_pickle=True)
+		else:
+			attributes = None
+		print(attributes)
+		return data, attributes
 
 def nc_read_write(write, output_filepath, dataset_name, data=None, qual_attrs=None):
 	if write:
@@ -36,6 +46,17 @@ def nc_read_write(write, output_filepath, dataset_name, data=None, qual_attrs=No
 		else:
 			ds = xr.Dataset({dataset_name:da})
 		ds.to_netcdf(output_filepath)
+	else:
+		ds = xr.open_dataset(output_filepath)
+		data = ds[dataset_name].values  # Assuming "labels" is the dataset name
+		if qual_attrs:
+			attributes = ds.attrs  # xarray stores attributes in the .attrs dictionary
+		else:
+			attributes = None
+		ds.close()
+
+		print(attributes)
+		return data, attributes
 
 def hdf_read_write(write, output_filepath, dataset_name, data=None, qual_attrs=None):
 	if write:
@@ -46,15 +67,25 @@ def hdf_read_write(write, output_filepath, dataset_name, data=None, qual_attrs=N
 				h5_dataset.attrs[key] = value
 		out_file.close()
 	else:
-		#TBD
-		return 0	
+		h5_dataset = h5.File(output_filepath, "r") 
+		data = h5_dataset[dataset_name][:]
+		if qual_attrs:
+			print(output_filepath.split('.')[0]+'_qual.npy')
+			attributes = {key: h5_dataset[dataset_name].attrs[key] for key in h5_dataset[dataset_name].attrs}
+		else:
+			attributes = None
+		h5_dataset.close()
+		print(attributes)
+		return data, attributes	
 
-def read_labels(output_filepath, dataset_name):
-
+def read_labels(output_filepath, dataset_name, qual_attrs=False):
+		#TODO : chcek that qual_file exists
 		filetype = output_filepath.split('.')[-1]
-
-                #if filetype == 'npy':
-                #elif filetype == 'hdf' or filtype=='hdf5':
-                #elif filetype == 'nc':
-                #else:
-                 #       raise Exception(f"filetype '{filetype}' id not currently supported for saving files.\n please use a different filetype for output, or add functionality for this filetype")
+		if filetype == 'npy':
+			return npy_read_write(False, output_filepath, dataset_name, qual_attrs=qual_attrs)
+		elif filetype == 'hdf' or filetype=='hdf5':
+			return hdf_read_write(False, output_filepath, dataset_name, qual_attrs=qual_attrs)
+		elif filetype == 'nc':
+			return nc_read_write(False, output_filepath, dataset_name, qual_attrs=qual_attrs)
+		else:
+			raise Exception(f"filetype '{filetype}' id not currently supported for reading files.\n please use a different filetype for output, or add functionality for this filetype")
