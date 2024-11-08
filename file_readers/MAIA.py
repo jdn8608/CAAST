e682bb1a3e7e79e08c12b0a3bf7403896dccd279
Joseph Nied <jdn8608@g.rit.edu>
2024-11-08 15:02:43 -0600

    fix

--- file_readers/MAIA.py
+++ file_readers/MAIA.py
@@ -18,7 +18,6 @@ def read(parent_dir, search, view,
 
 	
 	hdf_file = h5.File(search_result_files[0], 'r')
-	if bands_to_get == 'ALL':
 	if bands_to_get.upper() == 'ALL':
 		bands = np.array(list(hdf_file['Reflectance'].keys()))
 	else:	
