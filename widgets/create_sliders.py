68d8778e5e0dabf0c6a63a82352a5d38a7444f34
Joseph Nied <jdn8608@g.rit.edu>
2024-11-11 09:51:21 -0600

    create_sliders now returns created slider widget objects

--- widgets/create_sliders.py
+++ widgets/create_sliders.py
@@ -4,14 +4,18 @@ from widgets.LayerMinMaxSlider import LayerMinMaxSlider
 def create_sliders(option, viewer, layers=None, band_names=None, area='right'):
 	
 	if option == 0:
-		return
+		return None
 	elif option == 1:
 		slider_widget = SelectionMinMaxSlider(viewer)
 		viewer.window.add_dock_widget(slider_widget, name="Min-Max Range Slider", area=area)
+		return slider_widget
 	elif option == 2:
+		sliders = []
 		for i, layer in enumerate(layers):
 			if band_names[i] != "No Retrieval":
 				slider_widget = LayerMinMaxSlider(layer)
 				viewer.window.add_dock_widget(slider_widget, name=band_names[i], area=area)
+				sliders.append(slider_widget)
+		return sliders
 
 
