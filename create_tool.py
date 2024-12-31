73e7d62f50813a0204e07ef7ad85061ff943e45e
Joseph Nied <jdn8608@g.rit.edu>
2024-12-31 16:18:41 -0500

    Added initial editing layer of constant int value

--- create_tool.py
+++ create_tool.py
@@ -12,7 +12,16 @@ def add_layers(data_layer_dict, shape, viewer, config, load_labels_name=''):
     band_colormap, label_colormap, mask_colormap, nan_colormap = get_all_colormaps(
         config)
 
-    editing_data = None
+
+    # Check if the fill value is an int, if now, set-up for checking if layer to be
+    # filled by a instrument layer
+    try:
+        edit_data = np.zeros(data.shape, dtype=int) + int(load_labels_name)
+        edit_data_override = True 
+    except:
+        editing_data = None
+        edit_data_override = False
+
     for layer_name in data_layer_dict.keys():
         layer_type, data = data_layer_dict[layer_name]
 
@@ -22,7 +31,7 @@ def add_layers(data_layer_dict, shape, viewer, config, load_labels_name=''):
                              name=layer_name,
                              colormap=band_colormap)
 
-        # If not a image-type layer, process as labels
+        # If not an image-type layer, process as labels
         else:
             # Determine which color map to use for labels
             if layer_type is LayerType.CLOUD_MASK:
@@ -39,25 +48,27 @@ def add_layers(data_layer_dict, shape, viewer, config, load_labels_name=''):
             current_layer.editable = False  # do not allow for editing
 
             # Check to see if current layer is initial editing labels set by user
-            if layer_name.upper() == load_labels_name.upper():
-                editing_data = data[..., 0].astype(int)
+            if not edit_data_override and layer_name.upper() == load_labels_name.upper():
+                editing_data = data.astype(int)
 
     # Check to see if editing data was found... if not, store as zeros
-    if editing_data is None:
-        if load_labels_name:  # ambigous name was provided (not found)
-            warnings.warn(
-                "load_labels settings string was not found in the naming convetions"
-                "for the layers loaded. \nPlease refine the string entered you would "
-                "like initial labels loaded for editing.\nTo view the naming conventions"
-                " of the loaded layers, turn verbose on. \n "
-                "Loading zeros into the Editing Layer for now.",
-                category=UserWarning)
-        edit_data = np.zeros(data[..., 0].shape, dtype=int)
+    elif editing_data is None and load_labels_name:
+        # ambigous name was provided (not found)
+        warnings.warn(
+            "load_labels settings string was not found in the naming convetions"
+            "for the layers loaded. \nPlease refine the string entered you would "
+            "like initial labels loaded for editing.\nTo view the naming conventions"
+            " of the loaded layers, turn verbose on. \n "
+            "Loading zeros into the Editing Layer for now.",
+            category=UserWarning)
+        edit_data = np.zeros(data.shape, dtype=int)
 
     if load_labels_name:
-        editing_layer = viewer.add_labels(editing_data,
+        editing_layer = viewer.add_labels(editing_data[..., 0],
                                           name='Editing',
                                           colormap=label_colormap)
+
+    return editing_data
 
 
 def create_tool(data_layer_dict,
