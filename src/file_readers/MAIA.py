f934aa82a8fa57ea4ff053d5e28b13ca16d82f6e
Joseph Nied <jdn8608@g.rit.edu>
2025-06-17 15:22:07 -0500

    Adding MAIA RGB code
    Adding code to create true color composites for MAIA.
    
    Code is from Guangyu Zhao

--- src/file_readers/MAIA.py
+++ src/file_readers/MAIA.py
@@ -196,6 +196,125 @@ def create_nan_mask(band_data):
     return nan_mask, band_data
 
 
+# From Guangyu Zhao
+def bytescale(data, cmin=None, cmax=None, high=255, low=0):
+    """
+    Byte scales an array (image).
+
+    Byte scaling means converting the input image to uint8 dtype and scaling
+    the range to ``(low, high)`` (default 0-255).
+    If the input image already has dtype uint8, no scaling is done.
+
+    Parameters
+    ----------
+    data : ndarray
+        PIL image data array.
+    cmin : scalar, optional
+        Bias scaling of small values. Default is ``data.min()``.
+    cmax : scalar, optional
+        Bias scaling of large values. Default is ``data.max()``.
+    high : scalar, optional
+        Scale max value to `high`.  Default is 255.
+    low : scalar, optional
+        Scale min value to `low`.  Default is 0.
+
+    Returns
+    -------
+    img_array : uint8 ndarray
+        The byte-scaled array.
+
+    Examples
+    --------
+    >>> img = array([[ 91.06794177,   3.39058326,  84.4221549 ],
+                     [ 73.88003259,  80.91433048,   4.88878881],
+                     [ 51.53875334,  34.45808177,  27.5873488 ]])
+    >>> bytescale(img)
+    array([[255,   0, 236],
+           [205, 225,   4],
+           [140,  90,  70]], dtype=uint8)
+    >>> bytescale(img, high=200, low=100)
+    array([[200, 100, 192],
+           [180, 188, 102],
+           [155, 135, 128]], dtype=uint8)
+    >>> bytescale(img, cmin=0, cmax=255)
+    array([[91,  3, 84],
+           [74, 81,  5],
+           [52, 34, 28]], dtype=uint8)
+
+    """
+    if data.dtype == np.uint8:
+        return data
+
+    if high < low:
+        raise ValueError("`high` should be larger than `low`.")
+
+    if cmin is None:
+        cmin = np.nanmin(data.flatten())
+    if cmax is None:
+        cmax = np.nanmax(data.flatten())
+
+    cscale = cmax - cmin
+    if cscale < 0:
+        raise ValueError("`cmax` should be larger than `cmin`.")
+    elif cscale == 0:
+        cscale = 1
+
+    scale = float(high - low) / cscale
+    bytedata = (data * 1.0 - cmin) * scale + 0.4999
+    bytedata[bytedata > high] = high
+    bytedata[bytedata < 0] = 0
+    return np.asarray(bytedata, dtype=np.uint8) + np.uint8(low)
+
+
+# From Guangyu Zhao
+def get_enhanced_RGB(RGB):
+    """
+    Convert RGB BRFs to 8-bit color space [0,255]
+    """
+
+    def scale_image(image):
+        along_track = image.shape[0]
+        cross_track = image.shape[1]
+
+        x = np.array([0, 30, 60, 120, 190, 255], dtype=np.uint8)
+        y = np.array([0, 110, 160, 210, 240, 255], dtype=np.uint8)
+
+        scaled = np.zeros((along_track, cross_track), dtype=np.uint8)
+        for i in range(len(x) - 1):
+            x1 = x[i]
+            x2 = x[i + 1]
+            y1 = y[i]
+            y2 = y[i + 1]
+            m = (y2 - y1) / float(x2 - x1)
+            b = y2 - (m * x2)
+            mask = ((image >= x1) & (image < x2))
+            scaled = scaled + mask * np.asarray(m * image + b, dtype=np.uint8)
+
+        mask = image >= x2
+        scaled = scaled + (mask * 255)
+        return scaled
+
+    enhanced_RGB = np.zeros_like(RGB, dtype=np.uint8)
+    for i in range(3):
+        enhanced_RGB[:, :, i] = scale_image(bytescale(RGB[:, :, i]))
+    return enhanced_RGB
+
+
+def create_true_color(hdf_file):
+    ref06 = hdf_file['Reflectance/band_06'][()]
+    ref05 = hdf_file['Reflectance/band_05'][()]
+    ref04 = hdf_file['Reflectance/band_04'][()]
+    ref06[ref06 < 0] = np.nan
+    ref05[ref05 < 0] = np.nan
+    ref04[ref04 < 0] = np.nan
+    RGB = np.dstack((((ref06)), \
+        ((ref05)),((ref04))))
+
+    RGB[RGB == -999] = np.nan
+    RGB = get_enhanced_RGB(RGB)
+    return RGB
+
+
 def read(parent_dir, search, views, config=None):
     """Finds MAIA files and reads in required data for the tool
 
@@ -225,6 +344,7 @@ def read(parent_dir, search, views, config=None):
     add_dtt = config["add_dtt"]
     add_sid = config["add_surface_id"]
     add_geom = config["add_sun_view_geometry"]
+    add_true_color = True
 
     # Check what bands to load and how many bands
     if bands_to_get[0].upper() == 'ALL':
@@ -236,6 +356,8 @@ def read(parent_dir, search, views, config=None):
 
     # Intialize NumPy arrays
     band_data = np.zeros((Y_DIM, X_DIM, num_of_channels, len(views)))
+    if add_true_color:
+        rgb = np.zeros((Y_DIM, X_DIM, 3, len(views)))
     if add_cloud_mask:
         cloud_masks = np.zeros((Y_DIM, X_DIM, len(views)))
     if add_nan_mask:
@@ -271,8 +393,15 @@ def read(parent_dir, search, views, config=None):
         if band_names is None:
             band_names = np.array(list(hdf_file['Reflectance'].keys()))
 
+        print(band_names)
+
         # Get the band data
         band_data[..., i] = get_bands(hdf_file, band_names, num_of_channels)
+
+        # Create a true color composite from bands 4 5 6
+        if add_true_color:
+            rgb[..., i] = create_true_color(hdf_file)
+
         # Create a mask indicating where NaNs are found
         if add_nan_mask:
             nan_masks[..., i], band_data[...,
@@ -307,6 +436,10 @@ def read(parent_dir, search, views, config=None):
                                                                      i, :])
     # Get the band_data shape and cast as a list if a dim needs to be edited
     shape = list(band_data.shape)
+
+    # Add True Color Composite
+    if add_true_color:
+        data_layer_dict["True Color"] = (LayerType.RGB, rgb)
 
     # Add DTT and OBSERVABLES to the dict
     if add_dtt:
