55766d209e7b9859fefd1a805bbf7d8cdfff6109
Joseph Nied <jdn8608@g.rit.edu>
2025-08-04 11:19:26 -0500

    BugFix: Check for Min == Max... Perturb max if needed

--- src/widgets/ControlPanel.py
+++ src/widgets/ControlPanel.py
@@ -313,6 +313,11 @@ class ControlPanel(QFrame):
     def update_contrast_slider(self, layer):
         data_min, data_max = float(np.nanmin(layer.data)), float(
             np.nanmax(layer.data))
+
+        # Slightly perturb max value if min == max
+        if data_max == data_min:
+            data_max += np.power(0.1, 1 + np.floor(np.log10(data_max)))
+
         contrast_min, contrast_max = layer.contrast_limits
         self._contrast_data_min = data_min
         self._contrast_data_max = data_max
